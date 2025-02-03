51f723c2d1b265e721ba9b736cfedf70472f955c
rieadaits <rieadaits21@gmail.com>
2025-02-03 19:33:30 +0600

    implemented di

---
+++ lib/core/di/injection_container.dart
@@ -0,0 +1,39 @@
+import 'package:get_it/get_it.dart';
+import 'package:http/http.dart' as http;
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import '../../features/github_repos/data/datasources/github_remote_datasource.dart';
+import '../../features/github_repos/data/repositories/github_repository_impl.dart';
+import '../../features/github_repos/domain/repositories/github_repository.dart';
+import '../../features/github_repos/presentation/bloc/github_repos_bloc.dart';
+import '../network/network_info.dart';
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  // BLoC
+  sl.registerFactory(
+    () => GithubReposBloc(repository: sl()),
+  );
+
+  // Repository
+  sl.registerLazySingleton<GithubRepository>(
+    () => GithubRepositoryImpl(
+      remoteDataSource: sl(),
+      networkInfo: sl(),
+    ),
+  );
+
+  // Data sources
+  sl.registerLazySingleton<GithubRemoteDataSource>(
+    () => GithubRemoteDataSourceImpl(client: sl()),
+  );
+
+  // Core
+  sl.registerLazySingleton<NetworkInfo>(
+    () => NetworkInfoImpl(sl()),
+  );
+
+  // External
+  sl.registerLazySingleton(() => http.Client());
+  sl.registerLazySingleton(() => InternetConnectionChecker());
+} 
--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'core/di/injection_container.dart' as di;
+import 'features/github_repos/presentation/bloc/github_repos_bloc.dart';
+import 'features/github_repos/presentation/pages/home_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  di.init();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Popular GitRepos',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return BlocProvider(
+      create: (_) => di.sl<GithubReposBloc>(),
+      child: MaterialApp(
+        title: 'GitHub Repos',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+          useMaterial3: true,
         ),
+        home: const HomePage(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
     );
   }
 }
